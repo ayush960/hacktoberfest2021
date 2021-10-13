825d6977b57053c1d5c7e11939bcc9db1821840c
ayush960 <74626916+ayush960@users.noreply.github.com>
2021-10-13 19:14:16 +0530

    Update arrays.c
    I have added C code for 2d array, hoping its good to contribute, please look into it.

--- arrays.c
+++ arrays.c
@@ -1,8 +1,9 @@
-#include <stdio.h> 
+#include <stdio.h>         
 int main()
 {
     int n,i;
     int arr[100];
+    //for 1d array
     scanf("%d",&n);
     for (i=0;i<n;i++){
         scanf("%d",&arr[i]);
@@ -12,5 +13,24 @@ int main()
             printf("%d\n",arr[i]);
         }
     }
+    
+    //for 2d array
+   int disp[2][3];
+   int i, j;
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         printf("Enter value for disp[%d][%d]:", i, j);
+         scanf("%d", &disp[i][j]);
+      }
+   }
+   printf("Two Dimensional array elements:\n");
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         printf("%d ", disp[i][j]);
+         if(j==2){
+            printf("\n");
+         }
+      }
+   }
     return 0;
 }
